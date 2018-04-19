d7f71caeac15c1a8150db09b90322666bf93a184
itsme <itsme@DESKTOP-CJN20RS>
2018-04-19 11:33:38 +0530

    frontend till add product

---
+++ project1frontend/src/main/java/com/project/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package com.project.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class HomeController {
+	public HomeController(){
+		System.out.println("HomeController bean is created");
+	}
+
+	//This a handler method
+	//It can handle the request "http://localhost:8080/project1frontend/home -> DispatcherServlet -> "home"->
+	// /WEB-INF/views/home.jsp
+	@RequestMapping(value="/home")
+	public String getHomePage(){
+		return "home";
+	}
+	}
+
